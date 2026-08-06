4e278ae81ee44f2879fd1548fa09d268153c465f
Alishasaroha <raalisha4431@gmail.com>
2026-08-06 23:47:18 +0530

    Time: 54 ms (38.73%), Space: 51 MB (19.72%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,8 +4,9 @@ public:
         unordered_map<int, int> mpp;
         mpp[0] = 1;
         int count = 0;
+        int n =nums.size();
         int preSum = 0;
-        for(int i = 0; i < nums.size(); i++){
+        for(int i = 0; i < n; i++){
             preSum += nums[i];
             int remove = preSum - k;
             count = count + mpp[remove];
